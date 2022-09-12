c18bb67cb95b73e04b58fee3f8f2992648da581e
Javier Tortosa <americandepress6@gmail.com>
2022-09-13 00:38:44 +0200

    prueba git java

---
+++ com/contraseña/Inicio.java
@@ -0,0 +1,158 @@
+package com.contraseña;
+
+import java.util.Scanner;
+
+public class Inicio {
+
+	/**
+	 * 
+	 * @author Francisco Javier Tortosa
+	 * 
+	 * @implSpec
+	 * En este ejercicio debemos crear una clase que genere contraseñas aleatorias
+	 * compuestas por letras mayúsculas, minúsculas y números. Dichas contraseñas
+	 * serán de la longitud que quiera el usuario y dicha longitud se le pedirá por
+	 * teclado desde una ventana JOptionPane. Las contraseñas se almacenarán en un
+	 * Array cuyo tamaño también se le pedirá al usuario por teclado a través de una
+	 * ventana JOptionPane.
+	 * 
+	 * Recapitulando, al ejecutar el programa este pedirá al usuario el tamaño del
+	 * Array (imaginemos que el usuario introduce 4) y la longitud de las
+	 * contraseñas (imaginemos que el usuario introduce 7). El programa deberá
+	 * generar entonces 4 contraseñas de 7 caracteres cada una.
+	 * 
+	 * El programa imprimirá en consola todas las contraseñas generadas, el nº de
+	 * caracteres de cada una de ellas y si la contraseña es segura o débil en
+	 * función de la siguiente condición:
+	 * 
+	 * Se considerará contraseña segura aquella que esté formada por más de cinco
+	 * números, tenga más de una letra minúscula y más de dos letras mayúsculas.
+	 * 
+
+	 * Como siempre, hay infinitas formas de hacer esta aplicación, pero en este
+	 * caso se pide que el programa tenga:
+	 * 
+	 * Una clase que construya las contraseñas y una clase principal La clase que
+	 * construya las contraseñas deberá tener: Un constructor (diferente del
+	 * constructor por defecto) Dos métodos getters. Uno devolverá la contraseña
+	 * generada y el otro la longitud de la misma. Un método encargado de generar
+	 * (construir) la contraseña. Un método que evalúe y devuelva si la contraseña
+	 * es segura o débil en función de las condiciones antes descritas.
+	 * 
+	 * PARA REALIZAR ESTE EJERCICIO VAS A TENER QUE CONSULTAR LOS CÓDIGOS ASCII DE
+	 * LAS LETRAS DEL ABACEDARIO Y NÚMEROS DEL 1 AL 9. PUEDES ENCONTRARLOS AQUÍ:
+	 *
+	 
+	 * @category
+	 * Ejercicio de Practicas
+	 *
+	 
+	 * @link 
+	 * http://www.elcodigoascii.com.ar/codigos-ascii/letra-a-mayuscula-codigo-ascii-65.html
+	 *
+
+	 * @implNote
+	 * ascii
+	 * 48 - 57	numeros de 0-9 // 65 - 90 Letras A-Z // 97 - 122 Letras a-z
+	 *
+	
+	 * @implSpec
+	 * 
+	
+	 * @serialData 2022/08/29
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// iniciamos variables
+		
+		int cantidadDePass	= 10; 	// cantidad de pass que queremos 
+		int longitudPass	= 15;	// tamaño de cada pass 
+		int longitudPassVar = 0;	// para que cambie el tamaño de las pass o no
+		int longitudPassMax	= 15;	// tamaño de cada pass maximo
+		int longitudPassMin	= 5;	// tamaño de cada pass maximo
+		boolean seguro		= false;// si es seguro(true) o debil(false)
+		String txtSeguro	= "";	// texto de "seguro" o "debil"
+		
+		Passwords pass;				// instanciamos el objeto pass
+		
+		Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner para entrada por teclado
+		
+		// pedimos los datos por teclado
+		System.out.println("Introduzca cantidad de Passwords que desea generar:");
+		cantidadDePass = entradaEscaner.nextInt();
+		
+		System.out.println("Longitud de las paswords unica(0) o variable(1)");
+		longitudPassVar=entradaEscaner.nextInt();
+		
+		if (longitudPassVar==0) {
+			System.out.println("Introduzca el tamaño de cada Password: ");
+			longitudPass=entradaEscaner.nextInt();
+			longitudPassMax = longitudPass;
+			longitudPassMin = 0;
+		} else{
+			System.out.println("Introduzca el tamaño maximo de cada Password: ");
+			longitudPassMax=entradaEscaner.nextInt();
+			System.out.println("Introduzca el tamaño minimo de cada Password: ");
+			longitudPassMin=entradaEscaner.nextInt();
+		}
+			
+		
+			
+		System.out.println("");
+
+		//creamos el array con el tamaño que necesitamos
+		String passObtenido[][][]=new String[cantidadDePass][2][2];
+		
+		// iniciamos bucle para generar las claves
+		for (int i = 0; i<passObtenido.length; i++) {
+			//pass = new Passwords(longitudPass); // llamamos al constructor para que genere la clave
+			pass = new Passwords(longitudPassMax,longitudPassMin);
+			
+			seguro = pass.evaluaPass(pass.getPassGenerado()); // evaluamos si es segura o debil
+			
+			// convertimos el true o false de la comprobacion de las pass en "Segura" o "Debil"
+			if (seguro) {
+				txtSeguro="Segura";
+			} else { 
+				txtSeguro="Debil";
+			}
+
+			// rellenamos el array
+			passObtenido[i][0][0]=pass.getPassGenerado(); 					// metemos el pass
+			passObtenido[i][1][0]=String.valueOf(pass.getLongitudPass());	// metemos el tamaño
+			passObtenido[i][0][1]=txtSeguro;								// metemos Segura o debil
+		}
+		
+		// mostramos por pantalla el array con las pass y si es segura o debil
+		System.out.println("");
+		
+		String muestraPass; // clave que tiene espacios añadidos al final para verla bien por consola
+		
+		for (int i = 0; i<passObtenido.length; i++) {
+			muestraPass=passObtenido[i][0][0];
+
+			//ajustamos el tamaño de cada pass para que se vea bien por consola
+			if (muestraPass.length()<longitudPassMax) {
+				int sumaEspacios = longitudPassMax - muestraPass.length();
+				for (int j =0;j<sumaEspacios; j++) {
+					muestraPass += " ";
+				}	
+			}
+			if ( i < 9 ) {
+				System.out.print(" [");
+			} else {
+				System.out.print("[");
+			}
+				
+			System.out.println((i+1)+"]"+" Password: " + muestraPass +"\tTamaño: "+ passObtenido[i][1][0]
+					+"\tTipo: "+passObtenido[i][0][1]);
+			
+//			System.out.println("["+(i+1)+"]"+" Password: " + passObtenido[i][0][0] +"\tTamaño: "+ passObtenido[i][1][0]
+//					+"\tTipo: "+passObtenido[i][0][1]);
+		}
+		
+		entradaEscaner.close(); //cerramos el objeto scanner
+	}
+
+}
---
+++ com/contraseña/Passwords.java
@@ -0,0 +1,192 @@
+package com.contraseña;
+
+import java.util.Random;
+
+public class Passwords {
+
+	private String passGenerado;
+	private int longitudPass;
+	
+	/**
+	 * @param longitud: longitud de la variable
+	 */
+	public Passwords(int longitudMax, int longitudMin) {
+		this.passGenerado = generaPass(longitudMax, longitudMin);
+		this.longitudPass = passGenerado.length();
+	}
+
+	/**
+	 * constructor Standard
+	 */
+	public Passwords() {
+		
+	}
+	
+	/**
+	 * Generador de passwords del tamaño requerido
+	 * @return passGenerado
+	 */
+	public String generaPass(int longitudMax, int longitudMin) {
+		
+		String passGen="";
+		Random numeroRnd = new Random();
+		
+		int longitud = 0; 
+		
+		if(longitudMin!=0) {
+		
+			do { // este bucle repara el bug de generacion de la longitud fuera de limites
+
+//				longitud = (int) (Math.random()*longitudMax)+longitudMin;
+
+				longitud = numeroRnd.nextInt((longitudMax)+longitudMin);
+			}while (!(longitud>=longitudMin && longitud<=longitudMax));
+			
+		} else {
+			longitud = longitudMax;
+		}
+		
+		//System.out.println("longitudMax: "+longitudMax+" longitudMin: "+ longitudMin + " longitud: "+ longitud);
+		
+		for (int i=0; i<longitud; i++) {
+			passGen += generaCaracter(); // a1Dscferft435
+		}
+		return passGen;
+	}
+	
+	/**
+	 * Genera un caracter aleatorio entre numeros, mayusculas y minusculas
+	 * 
+	 * @return caracter
+	 */
+	public char generaCaracter() {
+		char caracter=' ';
+		
+		Random numeroRnd = new Random();
+		
+		int rndNumMayMin = numeroRnd.nextInt(3);// 0 numero 1 may 2 min
+		
+		//System.out.println("tipo: "+rndNumMayMin);
+		
+		int codigoAscii=0;
+		
+		switch (rndNumMayMin) {
+		case 0:
+			// numeros - entre 48 y 57 - 10
+			codigoAscii = 48 + numeroRnd.nextInt(10);
+			
+			//System.out.println("generado numero:" + codigoAscii);
+			break;
+			
+		case 1:
+			// mayusculas - entre 65 y 90 - 26
+			codigoAscii = 65 + numeroRnd.nextInt(26);
+			
+			//System.out.println("generada mayuscula:" + codigoAscii);
+			break;
+			
+		case 2:
+			// minusculas - entre 97 y 122 - 26
+			codigoAscii = 97 + numeroRnd.nextInt(26);
+			
+			//System.out.println("generada minuscula:" + codigoAscii);
+			break;
+		}
+		
+		caracter = (char) codigoAscii;
+		//System.out.println(caracter);
+		
+		return caracter;
+	}
+	
+	/**
+	 * controla si la pass generada es segura o debil
+	 * @return evaluacionPass
+	 */
+	public boolean evaluaPass(String pass) {
+		
+		// segura si contraseña > +5 numeros && +1 minusculas && +2 mayusculas
+		boolean evaluacionPass=false;
+		char desglose=' ';
+		int asciiDesglose=0;
+		int condicionNumero = 5;
+		int condicionMinusculas = 1;
+		int condicionMayusuculas = 2;
+		int may=0;
+		int min=0;
+		int num=0;
+		
+		
+		
+		for (int i=0; i<pass.length();i++) {
+			
+			desglose = pass.charAt(i);
+			
+			desglose = pass.charAt(i);
+			//System.out.println("desglose: "+ desglose);
+			
+			asciiDesglose = (int)desglose;
+			//System.out.println("asciiDesglose: "+asciiDesglose);
+			
+			if (asciiDesglose>= 48 && asciiDesglose <=57) {
+				num++;
+				//System.out.println("num: "+num);
+				
+			}
+			if (asciiDesglose>= 65 && asciiDesglose <=90) {
+				may++;
+				//System.out.println("may: "+may);
+				
+			}
+			if (asciiDesglose>= 97 && asciiDesglose <=122) {
+				min++;
+				//System.out.println("min: "+min);
+			}
+		}
+		
+		
+		/*
+		if (num>condicionNumero && min >condicionMinusculas && may >condicionMayusuculas) {
+			evaluacionPass = true;
+		} else {
+			evaluacionPass = false;
+		}
+		*/
+		evaluacionPass =(num>condicionNumero && min>condicionMinusculas &&
+				may >condicionMayusuculas) ? true : false;
+		
+		System.out.println("pass: "+pass + "\t+5 num: "+num+"\t+2 may: "+may+"\t+1 min: "+min
+				+"\tEvaluacion: "+evaluacionPass);
+		
+		return evaluacionPass;
+	}
+	
+	// ****************************************************************************************
+	// GETTERS Y SETTERS
+	
+	public String getPassGenerado() {
+		return passGenerado;
+	}
+
+	public void setPassGenerado(String passGenerado) {
+		this.passGenerado = passGenerado;
+	}
+
+	public int getLongitudPass() {
+		return longitudPass;
+	}
+
+	public void setLongitudPass(int longitudPass) {
+
+		this.longitudPass = longitudPass;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Passwords [passGenerado=" + passGenerado + ", longitudPass=" + longitudPass + "]";
+	}
+	
+	
+	
+}
